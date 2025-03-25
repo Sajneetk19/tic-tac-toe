9354c4f147483c7b396fffef8bf7c6537c8573b3
Sajneet kaur <sajneet1234.be21@chitkara.edi.in>
2025-03-25 11:47:15 +0530

    implemented functionality till winner declaration

--- src/Components/Square.jsx
+++ src/Components/Square.jsx
@@ -1,3 +1,8 @@
-export default function Square({ value ,onSquareClick}) {
-  return <button className="square" onClick={onSquareClick}>{value}</button>;
+export default function Square({ value, onSquareClick }) {
+  
+  return (
+    <button className="square" onClick={onSquareClick}>
+      {value}
+    </button>
+  );
 }
