081a245e55cdb2d2efeca671167409740cd6243c
Sajneetk1910 <your-email@example.com>
2025-03-27 13:04:17 +0530

    completed time travel functionality

--- src/Components/Game.jsx
+++ src/Components/Game.jsx
@@ -2,25 +2,28 @@ import { useState } from "react";
 import Board from "./Board";
 
 export default function Game() {
-  const [xIsNext, setXIsNext] = useState(true);
   const [history, setHistory] = useState([Array(9).fill(null)]);
-  const [currentMove, setCurrentMove] = useState(0);
   // [
-  //   [null, null, null, null, null, null, null, null, null],
-  //   [null, null, null, null, null, 'X', null, null, null],
-  //   [null, null, null, null, null, 'X', null, 'O', null],
-  // ]
-  // console.log('history', history);
-  const currentSquares = history[history.length - 1];
+    //   [null, null, null, null, null, null, null, null, null],
+    //   [null, null, null, null, null, 'X', null, null, null],
+    //   [null, null, null, null, null, 'X', null, 'O', null],
+    // ]
+    // console.log('history', history);
+    const [currentMove, setCurrentMove] = useState(0);
+    // const [xIsNext, setXIsNext] = useState(true);
+    const xIsNext = currentMove % 2 === 0;
+    const currentSquares = history[currentMove];
 
   function handlePlay(nextSquares) {
-    setHistory([...history, nextSquares]);
-    setXIsNext(!xIsNext);
+    const nextHistory = [...history.slice(0, currentMove + 1), nextSquares];
+    setHistory(nextHistory);
+    setCurrentMove(nextHistory.length - 1);
+    // setXIsNext(!xIsNext);
   }
 
   function jumpTo(nextMove) {
     setCurrentMove(nextMove);
-    setXIsNext(nextMove % 2 === 0);
+    // setXIsNext(nextMove % 2 === 0);
   }
 
   const moves = history.map((squares, move) => {
